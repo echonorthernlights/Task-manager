22495b3c63ed8daa85764a85365d95fb0f4d2aa8
NorthernLights <elhassanianas@gmail.com>
2021-12-21 22:41:24 +0100

    minorCommit

--- errors/customError.js
+++ errors/customError.js
@@ -1,11 +1,11 @@
-class customAPIError extends Error{
-    constructor(message, statusCode){
-        super(message)
-        this.statusCode = statusCode
-    }
+class customAPIError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
 const createCustomError = (message, statusCode) => {
-    return new customAPIError(message, statusCode)
-}
+  return new customAPIError(message, statusCode);
+};
 
-module.exports = {customAPIError, createCustomError}
+module.exports = { customAPIError, createCustomError };
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,10 +1,14 @@
-const {customAPIError} = require('../errors/customError')
+const { customAPIError } = require("../errors/customError");
 
 const errorHandler = (error, req, res, next) => {
-    if(error instanceof customAPIError){
-        return res.status(error.statusCode).json({msg:`Custom error handling (middleware + fct wrapper) ${error.message}`})
-    }
-    return res.status(500).json({msg:`Custom error : Someting went wrong , try again later`})
-}
+  if (error instanceof customAPIError) {
+    return res.status(error.statusCode).json({
+      msg: `Custom error handling (middleware + fct wrapper) ${error.message}`,
+    });
+  }
+  res
+    .status(500)
+    .json({ msg: `Custom error : Someting went wrong , try again later` });
+};
 
-module.exports = errorHandler
+module.exports = errorHandler;
